b5d81a3fe8f779d98e88d13578ef4042a23d39ed
mirenja <mirenjak@gmail.com>
2022-03-20 07:23:33 +0300

    pritf betty style

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+* main- entry point
+* Return: Always 0 (success)
+*
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a Piece of art,\n");
+	return (0);
+}
