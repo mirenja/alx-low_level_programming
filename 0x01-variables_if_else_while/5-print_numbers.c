378edcb232f44135838fbaadb5497e579bc6779f
mirenja <mirenjak@gmail.com>
2022-03-22 22:12:22 +0300

    numbers single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry Point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int s;
+
+	for (s = 0; s <= 10; s++)
+	{
+	putchar(s);
+	}
+	putchar('\n';
+return (0);
+}
