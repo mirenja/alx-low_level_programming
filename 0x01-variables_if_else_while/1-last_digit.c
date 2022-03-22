d3ab784b4008e20d3c9205655e046b9c00a6adb2
mirenja <mirenjak@gmail.com>
2022-03-22 05:45:51 +0300

    variable decare

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	Lastdigit = n % 10;
+	s = n % 10;
 	if (s > 5)
 	{
 	printf("Last digit of %d is %d and is  greater than 5\n", n, s);
