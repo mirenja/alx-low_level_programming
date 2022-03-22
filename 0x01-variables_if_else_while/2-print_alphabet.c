cf79b8df34f03c9888ae423a6f5f3259accb98d7
mirenja <mirenjak@gmail.com>
2022-03-22 18:44:07 +0300

    print alphaabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int s;
+
+	for (s = 'a';, s <= 'z'; s++)
+
+	{
+	putchar(s\n);
+	}
+return (0);
+}
