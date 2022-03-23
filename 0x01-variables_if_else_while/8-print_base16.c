4120d8d126c817251dbd57c19c453bbe072a1c2f
mirenja <mirenjak@gmail.com>
2022-03-23 15:09:57 +0300

    updated hex numbers

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 /**
-* main - main block
-* prints all base 16 in lowwercse
+* main- main block
+* prints all the hex numbers
 * Return: 0
 */
 int main(void)
 {
-	int i;
 	char c;
+	int i;
 	for (i = 0; i < 10; i++)
 	{
-	putchar(i + '0');
+	putchar(i);
 	}
-	for (c = 'a'; c < 'g'; c++)
+	for (c = 'a'; c = 'g'; c++)
 	{
 	putchar(c);
 	}
+putchar('\n');
 return (0);
 }
