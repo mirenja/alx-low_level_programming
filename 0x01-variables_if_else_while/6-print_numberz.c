1eab3577201e5bf2a7434e99e966fd6146c65d2f
mirenja <mirenjak@gmail.com>
2022-03-23 09:15:33 +0300

    use putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry Point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int n;
+	for (n == 0; n < 10; n++)
+	{
+	putchar("/d" ,n);
+	}
+return(0);
+}
