a643955debc9cbcc8d4c5b3c74e14ca21ff3f47b
mirenja <mirenjak@gmail.com>
2022-03-23 14:45:17 +0300

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - point
+* fuction to reverse a string
+*
+* Return: 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+	putchar(i);
+	}
+return (0);
+}
