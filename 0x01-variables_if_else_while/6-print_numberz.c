c2592610e247df6ba34624390a7bf464266b365c
mirenja <mirenjak@gmail.com>
2022-03-23 10:52:49 +0300

    update

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,12 @@
 */
 int main(void)
 {
-	int n = '0';
+	int n = "0";
 
-	for (n = '0'; n < '10'; n++)
+	for (n = "0"; n < "10"; n++)
 	{
 	putchar(n);
 	}
-putchar('\n');
+putchar("\n");
 return (0);
 }
