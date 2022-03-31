c253de37223ac1235b4792f67e9433b0b65b3255
mirenja <mirenjak@gmail.com>
2022-03-31 17:26:03 +0300

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* main - Entry point
+*
+* Return: 0
+*/
+void print_alphabet(void)
+{
+	char i = "a";
+
+	for (i = "a"; i <= "z"; i++)
+	{
+	_putchar("i");
+	}
+	putchar("\n");
+	Return(0);
+}
