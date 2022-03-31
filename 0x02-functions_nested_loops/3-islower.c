c2318eef275362a5d6a129e4e04ab8ec86deb73d
mirenja <mirenjak@gmail.com>
2022-03-31 21:04:19 +0300

    checks lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*
+* int _islower -checks lowercase
+* Return: 1 if lowercase
+* return: 0 if otherwise
+*/
+int _islower(int c)
+{
+	if ( c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
