faa1034a6ff01f5fd284acbf13fb9472915f7acf
mirenja <mirenjak@gmail.com>
2022-03-31 18:17:30 +0300

    changed the line ptchar("")

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,9 +8,9 @@ void print_alphabet(void)
 {
 	char i;
 
-	for (char i = "a"; i <= "z"; i++)
+	for (i = ' a'; i <= 'z'; i++)
 	{
-	_putchar("i");
+	_putchar(i);
 	}
 	putchar("\n");
 	Return(0);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 int _putchar(char c);
-void _print_alphabet(void);
+
+void print_alphabet(void);
 _
