75839cd02ffeee133057f4dc7c650ab58addd4d1
mirenja <mirenjak@gmail.com>
2022-04-01 12:28:06 +0300

    updated condition statement

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@ void jack_bauer(void)
 	int i;
 	int j;
 
-	for ( i = 00; i <= 23; i++)
+	for ( i = 00; i <= 24; i++)
 	{
-	for (j = 00; j <= 60; j++)
+	for (j = 00; j < 60; j++)
 	{
 	_putchar (i / 10 + '0');
 	_putchar (i % 10 + '0');	
