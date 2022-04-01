a4e9f1b77fcd7c3bc34c206c9671e3a7c5723315
mirenja <mirenjak@gmail.com>
2022-04-01 12:31:29 +0300

    update on betty

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,16 +8,16 @@ void jack_bauer(void)
 	int i;
 	int j;
 
-	for ( i = 00; i < 24; i++)
+	for (i = 00; i < 24; i++)
 	{
 	for (j = 00; j < 60; j++)
 	{
 	_putchar (i / 10 + '0');
-	_putchar (i % 10 + '0');	
+	_putchar (i % 10 + '0');
 	_putchar (':');
 	_putchar (j / 10 + '0');
-	_putchar (j % 10 + '0'); 
+	_putchar (j % 10 + '0');
 	_putchar ('\n');
 	}
-	} 
+	}
 }
