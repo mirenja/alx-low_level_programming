0e5281af9b5b4505c8be7b82b25b9259c929dc6c
mirenja <mirenjak@gmail.com>
2022-04-01 12:24:09 +0300

    double digits

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,9 +12,11 @@ void jack_bauer(void)
 	{
 	for (j = 00; j <= 60; j++)
 	{
-	_putchar (i + '0');
+	_putchar (i / 10 + '0');
+	_putchar (i % 10 + '0');	
 	_putchar (':');
-	_putchar (j + '0'); 
+	_putchar (j / 10 + '0');
+	_putchar (j % 10 + '0'); 
 	_putchar ('\n');
 	}
 	} 
