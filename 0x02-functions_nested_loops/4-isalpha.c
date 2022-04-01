cc6b840605d6811785a5573ee03d9d17d760c00f
mirenja <mirenjak@gmail.com>
2022-04-01 08:54:06 +0300

    checks for alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _isalpha - checks for alphabet
+* c: parameter to be checked
+* Return: 0 if alphabet
+* Return: 1 if otherwise
+*/
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || ((c >= 'A' && c <= 'Z'))
+	{
+	return (1);
+	{
+	else
+	{
+	return (0);
+	}
+}
