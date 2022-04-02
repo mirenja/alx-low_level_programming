4a5498351b9cd96577d5b02f81069651cd3a3ad0
mirenja <mirenjak@gmail.com>
2022-04-02 13:11:18 +0300

    prints natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_to_98 - prints from n to 98
+* Numbers seperate by a coma
+* use the std library
+* return: 0
+*/
+void print_to_98(int n)
+{
+	for (i == n; i <= 98; i++)
+	{
+	printf("%d\n",i);
+	} 
+}
+
