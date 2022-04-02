9a36ad94fa364197e8bb00221b845f2342b84cca
mirenja <mirenjak@gmail.com>
2022-04-02 14:27:47 +0300

    syntax update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-	for (n = n; n > 98; i--)
+	for (n = n; n > 98; n--)
 	{
 	printf("%d", n);
 	printf("%d\n", 98);
@@ -19,7 +19,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-	for (n = n; n <= 98; i++)
+	for (n = n; n <= 98; n++)
 	{
 	printf("%d", n);
 	printf("%d\n", 98);
