c1770f434c59b64bfaba4d2440a299cded7d3b70
mirenja <mirenjak@gmail.com>
2022-04-02 12:41:06 +0300

    trying to solve the last column space

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,11 +26,11 @@ void times_table(void)
 	_putchar((k / 10) + '0');
 	_putchar((k % 10) + '0');
 	}
-	else if (k <10 && j == 9)
+	else if (k < 10 && j == 9)
 	{
 	_putchar(' ');
-	_putchar(k + '0'); 
-	} 
+	_putchar(k + '0');
+	}
 	else
 	{
 	_putchar(' ');
