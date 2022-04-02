8824d10d2aa049bdec8d4716896d4ab58795fee5
mirenja <mirenjak@gmail.com>
2022-04-02 12:59:58 +0300

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add -  adds two integers and returns resUlt
+* @i: input int 1
+* @j: input para 2
+* Return: results
+*/
+int add(int i, int j)
+{
+	int i, j, k;
+
+	k = i + j;
+	return (k);
+}
