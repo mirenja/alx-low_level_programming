270bb28ffa89b37a8c094d5ecb9c9bbe7e249dc6
mirenja <mirenjak@gmail.com>
2022-04-02 14:24:52 +0300

    changed var

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,21 +9,19 @@
 */
 void print_to_98(int n)
 {
-	int i;
-
 	if (n > 98)
 	{
-	for (i = n; i > 98; i--)
+	for (n = n; i > 98; i--)
 	{
-	printf("%d", i);
+	printf("%d", n);
 	printf("%d\n", 98);
 	}
 	}
 	else
 	{
-	for (i = n; i <= 98; i++)
+	for (n = n; i <= 98; i++)
 	{
-	printf("%d", i);
+	printf("%d", n);
 	printf("%d\n", 98);
 	}
 	}
