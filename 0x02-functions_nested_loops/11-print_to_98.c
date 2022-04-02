c6c05dac4de14f07144b7c6a2627b25c9f3212a2
mirenja <mirenjak@gmail.com>
2022-04-02 14:02:16 +0300

    print 98 range

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,9 +11,28 @@ void print_to_98(int n)
 {
 	int i;
 
+	if (n > 98)
+	{
+	for (i = n; i >= 98; i--)
+	{
+		if (i != n)
+		{
+		printf(",");
+		}
+	printf("%d", i); 
+	} 
+	}
+	else
+	{
 	for (i = n; i <= 98; i++)
 	{
-	printf("%d, ", i);
-	}
+		if (i != n)
+		{
+		printf(",");
+		}	
+	printf("%d", i);
+	}/*cloing for second if8*/
+	}/*else closing*/
+	_putchar('\n');	
 }
 
