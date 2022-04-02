67398b8e67b48dcc2b61c840a9c50f6fdfba6e8d
mirenja <mirenjak@gmail.com>
2022-04-02 14:13:08 +0300

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,18 +15,18 @@ void print_to_98(int n)
 	{
 	for (i = n; i > 98; i--)
 	{
-	printf("%d", i); 
+	printf("%d", i);
 	printf("%d\n", 98);
-	} 
+	}
 	}
 	else
 	{
-	for (i = n; i < 98; i++)
+	for (i = n; i <= 98; i++)
 	{
 	printf("%d", i);
 	printf("%d\n", 98);
-	}/*cloing for second if8*/
-	}/*else closing*/
-	_putchar('\n');	
+	}
+	}
+	_putchar('\n');
 }
 
