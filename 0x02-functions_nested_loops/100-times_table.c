5e0d645d9350b693f257a451e97b1b4f523701be
mirenja <mirenjak@gmail.com>
2022-04-04 13:00:04 +0300

    first attempt multiple table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+* times_table - Multiplication table for nine
+* Return: 0
+*/
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	if ( n > 15 || n < 0)
+	{
+	break;
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+		for (j = 0; j <= n; j++)
+		{
+		k = i * j;
+		if (k < 10)
+		{
+		_putchar(k + '0');
+		}
+		else
+		{
+		_putchar((k / 10) + '0');
+		_putchar((k % 10) + '0');	
+		} 
+		} 
+		_putchar('\n');
+		}
+	}
+}
