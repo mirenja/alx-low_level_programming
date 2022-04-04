485e7674d6dfd72801db9d74e39a552e6475bf7b
mirenja <mirenjak@gmail.com>
2022-04-04 09:03:57 +0300

    syntax error i--

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 
 	if (n <  98)
 	{
-	for (i == n; i < 98; i++)
+	for (i = n; i < 98; i++)
 	{
 	printf("%d,", n);
 	}
@@ -24,7 +24,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-	for (i = n; i >  98; --)
+	for (i = n; i >  98; i--)
 	{
 	printf("%d, ", n);
 	}
