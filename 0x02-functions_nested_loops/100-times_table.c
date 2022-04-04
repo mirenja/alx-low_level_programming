a6271be64722e2fcb06a4beab266b46b1eb1f557
mirenja <mirenjak@gmail.com>
2022-04-04 13:33:47 +0300

    updated betty

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,21 +1,22 @@
 #include "main.h"
 /**
-* times_table - Multiplication table for nine
+* print_times_table - Multiplication table for nine
+* @n: int value set
 * Return: 0
 */
 void print_times_table(int n)
 {
 	int i, j, k;
-		
+
 	for (i = 0; i <= n; i++)
 	{
 	for (j = 0; j <= n; j++)
 	{
 	k = i * j;
-	if ( n >15 || n < 0)
+	if (n > 15 || n < 0)
 	{
 	continue;
-	} 
+	}
 	else if (k < 10)
 	{
 	_putchar(k + '0');
@@ -23,10 +24,9 @@ void print_times_table(int n)
 	else
 	{
 	_putchar((k / 10) + '0');
-	_putchar((k % 10) + '0');	
-	} 
-	} 
-	_putchar('\n');
+	_putchar((k % 10) + '0');
 	}
+	}
+	_putchar('\n');
 	}
 }
