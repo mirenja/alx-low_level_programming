9bcba2eb827f141af1c5fca66d390aeb06480824
mirenja <mirenjak@gmail.com>
2022-04-04 13:26:53 +0300

    swapped the loop

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,29 +6,27 @@
 void print_times_table(int n)
 {
 	int i, j, k;
-
-	if ( n > 15 || n < 0)
+		
+	for (i = 0; i <= n; i++)
+	{
+	for (j = 0; j <= n; j++)
+	{
+	k = i * j;
+	if ( n >15 || n < 0)
 	{
 	continue;
+	} 
+	else if (k < 10)
+	{
+	_putchar(k + '0');
 	}
 	else
 	{
-		for (i = 0; i <= n; i++)
-		{
-		for (j = 0; j <= n; j++)
-		{
-		k = i * j;
-		if (k < 10)
-		{
-		_putchar(k + '0');
-		}
-		else
-		{
-		_putchar((k / 10) + '0');
-		_putchar((k % 10) + '0');	
-		} 
-		} 
-		_putchar('\n');
-		}
+	_putchar((k / 10) + '0');
+	_putchar((k % 10) + '0');	
+	} 
+	} 
+	_putchar('\n');
+	}
 	}
 }
