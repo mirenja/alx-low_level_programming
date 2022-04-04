5259afbd69b5e37dbfa66fd132ba4312ba18933d
mirenja <mirenjak@gmail.com>
2022-04-04 13:42:04 +0300

    trailing white space

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,19 +12,23 @@ void print_times_table(int n)
 	{
 	for (j = 0; j <= n; j++)
 	{
-	k = i * j;
 	if (n > 15 || n < 0)
 	{
-	continue;
+	break;
 	}
 	else if (k < 10)
 	{
+	k = i * j;
 	_putchar(k + '0');
+	_putchar(',');
+	_putchar(' ');
 	}
 	else
 	{
 	_putchar((k / 10) + '0');
 	_putchar((k % 10) + '0');
+	_putchar(',');
+	_putchar(' ');
 	}
 	}
 	_putchar('\n');
